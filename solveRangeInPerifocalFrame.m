d2bbc04d7f5372b809191db9a12c9dd555d9e835
19011228lyh <133726994+19011228lyh@users.noreply.github.com>
2023-06-17 04:02:46 +0900

    Delete solveRangeInPerifocalFrame.m

--- solveRangeInPerifocalFrame.m
+++
@@ -1,6 +0,0 @@
-function rangeInPQW = solveRangeInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
-true_anomaly_rad=pi/180*true_anomaly;
-r=semimajor_axis*(1-eccentricity^2)/(1+eccentricity*cos(true_anomaly_rad));
-r_sn=[r ; 0 ; 0]; 
-rangeInPQW=[cos(true_anomaly_rad) -sin(true_anomaly_rad) 0; sin(true_anomaly_rad) cos(true_anomaly_rad) 0; 0 0 1]*r_sn;
-end %모든 값 단위 [km]
